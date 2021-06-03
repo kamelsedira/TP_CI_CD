bf77bb146e8ee4808167e348029837515b9892f1
Kamel_Sedira <ksedira@outlook.com>
2021-06-03 17:56:08 +0200

    prjet CI_CD initial 3

--- install-python.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update
-sudo apt upgrade 
-
-sudo apt remove -y python
-# install python3
-sudo apt-get install python3 python3-dev python3-pip -q -y
-sudo mv /usr/bin/python3 /usr/bin/python
